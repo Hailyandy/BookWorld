a5ce45333dc3b172327ade66e7dc7f507995625e
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2023-09-23 17:56:07 +0700

    structure project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import  { useState } from 'react';
+
+import { Button } from 'antd';
+
+
+function App() {
+  return (
+    <div class ="App">
+      <header className="App-header">
+        <Button>Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
