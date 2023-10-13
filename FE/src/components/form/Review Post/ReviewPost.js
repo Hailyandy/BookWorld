5ebeb7caf5c85299311874a5a8fb2bb344f4e349
HoagHaiLy <larissa00001852@gmail.com>
2023-10-14 06:55:23 +0700

    book world

--- FE/src/components/form/Review Post/ReviewPost.js
+++ FE/src/components/form/Review Post/ReviewPost.js
@@ -1,8 +1,7 @@
 import AvartarTime from "components/ui/AvartarTime/AvartarTime"
 import "./reviewPost.css"
 import { StarFilled } from '@ant-design/icons';
-import { DownloadOutlined } from '@ant-design/icons';
-import { Button, Radio, Space, Divider } from 'antd';
+import ContentIntro from "../Content Intro/ContentIntro";
 
 
 
@@ -26,34 +25,7 @@ const ReviewPost = () => {
 
             {/* Body Post */}
 
-            <div class = "body-post">
-                <div class = "bookjacket-intro">
-                    <span style={{
-                        width: "120px",
-                        height: "120px"
-                    }} 
-                    class = "bookjacket-image"></span>
-                    <Button style={{
-                        backgroundColor: "#43d8cd",
-                        marginTop : "20px"
-                    }}
-                    type="primary" shape="round" >
-                       Muốn đọc
-                    </Button>
-                    <Button style={{
-                        backgroundColor: "#43d8cd",
-                        marginTop : "20px"
-                    }}
-                    type="primary" shape="round"  >
-                       Thảo luận
-                    </Button>
-                </div>
-                <div class = "book-content-intro">
-                    <h2 class = "title">Hôm nay tôi thất tình</h2>
-                    <h3 class = "author">Hạ Vũ</h3>
-                    <p class = "content-intro">industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lore</p>
-                </div>
-            </div>
+            <ContentIntro></ContentIntro>
 
         </div>
     )
--- FE/src/pages/FristHome/FristHome.js
+++ FE/src/pages/FristHome/FristHome.js
@@ -1,12 +1,12 @@
 import "./fristhome.css"
 import React, { useState } from 'react';
-import { Button, Radio, Space, Divider } from 'antd';
+import { Button } from 'antd';
 import imgApp from '../assets/imgs/App Store.png'
 import imgPlay from '../assets/imgs/Google Play.png'
 
 
 const FristHome = () => {
-    const [size, setSize] = useState('large');    return ( 
+    return ( 
         <div className="home-container">
             <div className="content">
             <h1>Xin chào bạn đến với Book World</h1>
@@ -14,11 +14,11 @@ const FristHome = () => {
             
             <Button style={{
                             backgroundColor: "#00c0a6",
-                            width: "191px",
+                            width: "250px",
                             height: "64px",
                             fontSize: "20px",
                         }}
-            type="primary" shape="round"  size={size}>
+            type="primary" shape="round"  >
             Đăng ký
           </Button>
           <div class = "app_connect">
