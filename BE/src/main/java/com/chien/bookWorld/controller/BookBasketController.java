d6c6a7427e71b10b6fc5728232967dcdce8ee6ce
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2023-10-21 14:28:26 +0700

    sua loi BE

--- BE/src/main/java/com/chien/bookWorld/controller/BookBasketController.java
+++ BE/src/main/java/com/chien/bookWorld/controller/BookBasketController.java
@@ -13,14 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
 @RestController
 @RequestMapping("/api/bookBasket")
 @SecurityRequirement(name = "javainuseapi")
