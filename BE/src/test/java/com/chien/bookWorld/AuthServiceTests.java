7905fc5ae404c5d1889c54e9e6a3a45221c2a4a4
Nguyễn Minh Chiến <chien9pm@gmail.com>
2023-11-22 17:28:58 +0700

    H2 BE test

--- BE/src/test/java/com/chien/bookWorld/AuthServiceTests.java
+++ BE/src/test/java/com/chien/bookWorld/AuthServiceTests.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AuthServiceTests {
+class AuthServiceTests {
 
 //  @TestConfiguration
 //  public static class AuthServiceTestConfiguration{
