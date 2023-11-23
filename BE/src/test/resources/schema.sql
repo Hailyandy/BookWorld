3654ea1586d5bde5a5bc5b384a25c54e28ac186d
Nguyễn Minh Chiến <chien9pm@gmail.com>
2023-11-23 20:34:14 +0700

    Add test login

--- BE/src/test/resources/schema.sql
+++ BE/src/test/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA `bookworld` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- CREATE SCHEMA `bookworld` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `bookworld`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
