b08e68ad92d5eb6ea0c79b132438fffcb68ff95a
Nguyễn Minh Chiến <chien9pm@gmail.com>
2023-12-06 22:12:57 +0700

    Add test controller login

--- BE/src/test/java/com/chien/bookWorld/AuthControllerTests.java
+++ BE/src/test/java/com/chien/bookWorld/AuthControllerTests.java
@@ -5,7 +5,9 @@ import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.IS
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import com.chien.bookWorld.controller.AuthController;
+import com.chien.bookWorld.exception.AppException;
 import com.chien.bookWorld.payload.request.LoginRequest;
+import com.chien.bookWorld.payload.response.AppExceptionBody;
 import com.chien.bookWorld.payload.response.SuccessResponse;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -13,6 +15,8 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -64,11 +68,6 @@ class AuthControllerTests {
 //1
   @Test
 //  @Sql(scripts = "/schema.sql")
-  @Sql(
-      scripts = "/drop.sql",
-      config = @SqlConfig(transactionMode = ISOLATED),
-      executionPhase = AFTER_TEST_METHOD
-  )
   public void testLoginSuccess() throws Exception {
     MvcResult mvcResult = mvc.perform(
         post("/api/auth/signin").contentType(MediaType.APPLICATION_JSON)
@@ -80,6 +79,68 @@ class AuthControllerTests {
     String content = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
     SuccessResponse response = mapFromJson(content, SuccessResponse.class);
     Assert.assertEquals("Thành công!", response.getMessage());
+  }
+  //2
+  @Test
+  public void testLoginUsernameNotFound() throws Exception {
+    MvcResult mvcResult = mvc.perform(
+        post("/api/auth/signin").contentType(MediaType.APPLICATION_JSON)
+            .content(mapToJson(new LoginRequest("chien5pm@gmail.com", "123456")))).andReturn();
+
+    int status = mvcResult.getResponse().getStatus();
+    Assert.assertEquals(404, status);
+
+    String content = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+    AppExceptionBody response = mapFromJson(content, AppExceptionBody.class);
+//    Assert.assertEquals("Thành công!", response.getMessage());
+
+    String expectedMessage = "Lỗi: Không tồn tại! Không tìm thấy tài khoản có tên '";
+    String actualMessage = response.getMessage();
+
+    Assert.assertTrue(actualMessage.contains(expectedMessage));
+  }
+  //3
+  @Test
+  public void testLoginNotEnabled() throws Exception {
+    MvcResult mvcResult = mvc.perform(
+        post("/api/auth/signin").contentType(MediaType.APPLICATION_JSON)
+            .content(mapToJson(new LoginRequest("chien9pmt@gmail.com", "12345678")))).andReturn();
+
+    int status = mvcResult.getResponse().getStatus();
+    Assert.assertEquals(403, status);
+
+    String content = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+    AppExceptionBody response = mapFromJson(content, AppExceptionBody.class);
+//    Assert.assertEquals("Thành công!", response.getMessage());
+
+    String expectedMessage = "Tài khoản của bạn chưa được quản trị viên phê duyệt, vui lòng đợi!";
+    String actualMessage = response.getMessage();
+
+    Assert.assertTrue(actualMessage.contains(expectedMessage));
+  }
+  //3
+  @Test
+  @Sql(
+      scripts = "/drop.sql",
+      config = @SqlConfig(transactionMode = ISOLATED),
+      executionPhase = AFTER_TEST_METHOD
+  )
+  public void testLoginIncorrectPassword() throws Exception {
+    MvcResult mvcResult = mvc.perform(
+        post("/api/auth/signin").contentType(MediaType.APPLICATION_JSON)
+            .content(mapToJson(new LoginRequest("chien9pm@gmail.com", "123456789")))).andReturn();
+
+    int status = mvcResult.getResponse().getStatus();
+    Assert.assertEquals(401, status);
+
+    String content = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+    AppExceptionBody response = mapFromJson(content, AppExceptionBody.class);
+//    Assert.assertEquals("Thành công!", response.getMessage());
+
+    String expectedMessage = "Sai mật khẩu!";
+    String actualMessage = response.getMessage();
+
+    Assert.assertTrue(actualMessage.contains(expectedMessage));
   }
 
   @AfterAll
