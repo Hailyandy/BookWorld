d8efef5fd6d28bf215c9b40dc3f2a22951ca0509
Nguyễn Minh Chiến <chien9pm@gmail.com>
2023-12-09 05:56:03 +0700

    fix

--- BE/src/main/java/com/chien/bookWorld/entity/Book.java
+++ BE/src/main/java/com/chien/bookWorld/entity/Book.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,6 +25,7 @@ import lombok.Setter;
 import lombok.ToString;
 import org.checkerframework.common.aliasing.qual.Unique;
 import jakarta.validation.constraints.NotNull;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -31,8 +33,9 @@ import jakarta.validation.constraints.NotNull;
 @Setter
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Book {
+
   @Id
-@Column(unique = true, nullable = false)
+  @Column(unique = true, nullable = false)
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
@@ -54,7 +57,7 @@ public class Book {
   @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
   private Set<BookBasket> bookBaskets;
 
-  @ManyToMany
+  @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "book_genre", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "genre_id"))
   private Set<Genre> genres;
 
