28412d15a03ef3e9684f81b99ca73b25e1318aad
Nguyễn Minh Chiến <chien9pm@gmail.com>
2023-12-08 16:00:10 +0700

    Thêm test book basket

--- BE/src/main/java/com/chien/bookWorld/dto/BookBasketUpdateDto.java
+++ BE/src/main/java/com/chien/bookWorld/dto/BookBasketUpdateDto.java
@@ -2,11 +2,15 @@ package com.chien.bookWorld.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookBasketUpdateDto {
 
   @NotNull(message = "Thiếu ID sách!")
