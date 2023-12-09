8656885c448d0b049b9054e739c0df668026cb67
Nguyễn Minh Chiến <chien9pm@gmail.com>
2023-12-09 07:24:01 +0700

    Fix OTP

--- BE/src/main/java/com/chien/bookWorld/entity/User.java
+++ BE/src/main/java/com/chien/bookWorld/entity/User.java
@@ -45,11 +45,11 @@ public class User {
     this.password = password;
   }
 
-  @ManyToMany
+  @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roles;
 
-  @ManyToMany
+  @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "likes", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "post_id"))
   private Set<Post> posts;
 
